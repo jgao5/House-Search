fc6479a25ecd50df7efadabec37f7033508ea674
jgao5 <gaojian9@outlook.com>
2018-09-05 17:12:22 +0800

    Initial commit

---
+++ src/main/java/com/jian/housesearch/HouseSearchApplication.java
@@ -0,0 +1,12 @@
+package com.jian.housesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseSearchApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jian/housesearch/HouseSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jian.housesearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HouseSearchApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
